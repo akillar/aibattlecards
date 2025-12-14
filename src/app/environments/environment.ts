d4c2437214cce22385f22f25a27fb158f27c1438
ASK <akillar@yahoo.com>
2025-12-14 20:20:37 +0530

    api-url

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,7 @@
  
   export const environment = {
   production: true,
-  APIURL: 'http://127.0.0.1:8000/',
+  APIURL: '/api/',
   idforauthafteruserloggedin: 'Hiwql329dwqldkqwNlwqfqnfqwfewnlgwKlmfewl88kndwqfnlwj21fewnmnfewPpowqpdwqmMpMDWFMNEWhHkdwlkkkkfnfewlkFEWFEWdwdrewgregre',
   isforauth_without_userloggedin: 'fehwkjfnvrkewjnhHGuyiuewfLkjlkfjewfle3213iuoieLKjlKJLFEJWLFKJWEFNNMkljflkewjflkwejflkwejflkwejflkwejflkwejflkwejflkwejflkwejf',
 };
